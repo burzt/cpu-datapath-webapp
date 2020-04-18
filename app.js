2402a0303aba4588aacfb3a81813af8b21fb9041
Daniel Carpenter <d_c176@txstate.edu>
2020-04-18 11:46:57 -0500

    adds line following cursor to datapath.html

---
+++ app.js
@@ -0,0 +1,45 @@
+var rootStage = new PIXI.Container();
+var renderer = PIXI.autoDetectRenderer(512,512, {transparent: true, resolution: 1});
+var window = document.getElementById('display');
+
+document.getElementById('display').appendChild(renderer.view);
+
+PIXI.utils.sayHello();
+
+class Line extends PIXI.Graphics {
+    constructor(points, lineSize, lineColor) {
+        super();
+        
+        var s = this.lineWidth = lineSize || 5;
+        var c = this.lineColor = lineColor || "0xF00F0F";
+        
+        this.points = points;
+
+        this.lineStyle(s, c)
+
+        this.moveTo(points[0], points[1]);
+        this.lineTo(points[2], points[3]);
+    }
+    
+    updatePoints(p) {
+        
+        var points = this.points = p.map((val, index) => val || this.points[index]);
+        
+        var s = this.lineWidth, c = this.lineColor;
+        
+        this.clear();
+        this.lineStyle(s, c);
+        this.moveTo(points[0], points[1]);
+        this.lineTo(points[2], points[3]);
+        renderer.render(rootStage);
+    }
+}
+
+var line = new Line([200, 50, 0, 0]);
+rootStage.addChild(line);
+
+renderer.render(rootStage);
+
+window.addEventListener("mousemove", e => line.updatePoints([null, null, e.clientX, e.clientY]), false);
+
+
