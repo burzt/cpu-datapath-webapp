e9de7e44e76d1861606c740cb622844e17d66dc9
Aaron Elam <speztra97@gmail.com>
2020-05-06 19:59:49 -0500

    Drawing register component

---
+++ index.js
@@ -0,0 +1,101 @@
+function init() {
+  var $ = go.GraphObject.make;
+  myDiagram = $(go.Diagram, "myDiagramDiv")
+
+
+  myDiagram.nodeTemplate =
+    // a spot is literally a spot on the shape
+    $(go.Node, "Spot",
+      $(go.Panel, "Auto",
+        // register
+        $(go.Shape, "RoundedRectangle", {
+          fill: "lightblue",
+          desiredSize: new go.Size(100, 150)
+        }),
+        $(go.TextBlock, { margin: 10 },
+          new go.Binding("text", "key")
+        )
+      ),
+      // readreg1 left port
+      $(go.Shape, "Ellipse",
+        {
+          fill: "pink",
+          // puts readreg1 top left
+          alignment: new go.Spot(0, 0, 0, 30),
+          desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+          portId: "l", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+          fromLinkable: false, toLinkable: true
+        }
+      ),
+      // readreg2 left port
+      $(go.Shape, "Ellipse",
+        {
+          fill: "pink",
+          // puts readreg1 top left
+          alignment: new go.Spot(0, 0, 0, 60),
+          desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+          portId: "l", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+          fromLinkable: false, toLinkable: true
+        }
+      ),
+      // write register left port
+      $(go.Shape, "Ellipse",
+        {
+          fill: "pink",
+          // puts readreg1 top left
+          alignment: new go.Spot(0, 0, 0, 90),
+          desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+          portId: "l", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+          fromLinkable: false, toLinkable: true
+        }
+      ),
+      // write data left port
+      $(go.Shape, "Ellipse",
+        {
+          fill: "pink",
+          // puts readreg1 top left
+          alignment: new go.Spot(0, 0, 0, 120),
+          desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+          portId: "l", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+          fromLinkable: false, toLinkable: true
+        }
+      ),
+      // read data1 right port
+      $(go.Shape, "Ellipse",
+        {
+          fill: "green",
+          alignment: new go.Spot(1, 0, 0, 45),
+          desiredSize: new go.Size(10, 10), //alignment: go.Spot.Right,
+          portId: "r", fromSpot: go.Spot.Left, toSpot: go.Spot.Left,
+          fromLinkable: true, toLinkable: false
+        }
+      ),
+      // read data2 right port
+      $(go.Shape, "Ellipse",
+        {
+          fill: "green",
+          alignment: new go.Spot(1, 0, 0, 105),
+          desiredSize: new go.Size(10, 10), //alignment: go.Spot.Right,
+          portId: "r", fromSpot: go.Spot.Left, toSpot: go.Spot.Left,
+          fromLinkable: true, toLinkable: false
+        }
+      ),
+      // $(go.TextBlock, { font: "20px sans-serif" },
+      //   new go.Binding("text", "key")
+      // )
+    );
+
+  myDiagram.model = new go.GraphLinksModel(
+    [
+      { key: "Register" },
+      { key: "Beta" },
+    ],
+    [
+      // this makes the line before the user can. Maybe we call this for the solver
+      // { from: "Register", to: "Beta", fromPort: "r", toPort: "l" }
+    ]
+  );
+
+  myDiagram.model.linkFromPortIdProperty = "fromPort";
+  myDiagram.model.linkToPortIdProperty = "toPort";
+}
