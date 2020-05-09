a2249742e8251dc6806868d123ddf940b811eb84
Aaron Elam <speztra97@gmail.com>
2020-05-08 21:39:26 -0500

    Register labels added

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function loadInstruction() {
   myDiagram.nodeTemplate =
     // a spot is literally a spot on the shape
     $(go.Node, "Spot",
-      $(go.Panel, "Auto",
+      $(go.Panel, "Spot",
         // register
         $(go.Shape, "RoundedRectangle", {
           fill: "lightblue",
           desiredSize: new go.Size(100, 150),
           //initialDocumentSpot: go.Spot.TopCenter
         }),
-        $(go.TextBlock, { margin: 10 },
+        $(go.TextBlock, { margin: 0, position: new go.Point(50, 75) },
           new go.Binding("text", "key")
         )
       ),
@@ -26,41 +26,45 @@ function loadInstruction() {
           desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
           portId: "readreg1", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
           fromLinkable: false, toLinkable: true
-        }
+        },
       ),
+      $(go.TextBlock, "RR1", { alignment: new go.Spot(0, 0, 5, 30), alignmentFocus: go.Spot.Left }),
       // readreg2 left port
       $(go.Shape, "Ellipse",
         {
           fill: "pink",
-          // puts readreg1 top left
+          // puts readreg2 top left
           alignment: new go.Spot(0, 0, 0, 60),
           desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
           portId: "readreg2", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
           fromLinkable: false, toLinkable: true
         }
       ),
+      $(go.TextBlock, "RR2", { alignment: new go.Spot(0, 0, 5, 60), alignmentFocus: go.Spot.Left }),
       // write register left port
       $(go.Shape, "Ellipse",
         {
           fill: "pink",
-          // puts readreg1 top left
+          // puts write register top left
           alignment: new go.Spot(0, 0, 0, 90),
           desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
           portId: "writereg", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
           fromLinkable: false, toLinkable: true
         }
       ),
+      $(go.TextBlock, "WR", { alignment: new go.Spot(0, 0, 5, 90), alignmentFocus: go.Spot.Left }),
       // write data left port
       $(go.Shape, "Ellipse",
         {
           fill: "pink",
-          // puts readreg1 top left
+          // puts write data top left
           alignment: new go.Spot(0, 0, 0, 120),
           desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
           portId: "writedata", fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
           fromLinkable: false, toLinkable: true
         }
       ),
+      $(go.TextBlock, "WD", { alignment: new go.Spot(0, 0, 5, 120), alignmentFocus: go.Spot.Left }),
       // read data1 right port
       $(go.Shape, "Ellipse",
         {
@@ -84,53 +88,78 @@ function loadInstruction() {
       // $(go.TextBlock, { font: "20px sans-serif" },
       //   new go.Binding("text", "key")
       // )
+    ),
+    // $(go.Panel, "Spot",
+    //   // register
+    //   $(go.Shape, "TriangleRight", {
+    //     fill: "lightred",
+    //     desiredSize: new go.Size(100, 150),
+    //     //initialDocumentSpot: go.Spot.TopCenter
+    //   }),
+    //   $(go.TextBlock, { margin: 0, position: new go.Point(50, 75) },
+    //     new go.Binding("text", "key")
+    //   )
+    // ),
+    myDiagram.model = new go.GraphLinksModel(
+      [
+        { key: "Register" },
+        { key: "Beta" },
+      ],
+      [
+        // this makes the line before the user can. Maybe we call this for the solver
+        // { from: "Register", to: "Beta", fromPort: "r", toPort: "l" }
+      ]
     );
-
-  myDiagram.model = new go.GraphLinksModel(
-    [
-      { key: "Register" },
-      { key: "Beta" },
-    ],
-    [
-      // this makes the line before the user can. Maybe we call this for the solver
-      // { from: "Register", to: "Beta", fromPort: "r", toPort: "l" }
-    ]
-  );
 
   myDiagram.model.linkFromPortIdProperty = "fromPort";
   myDiagram.model.linkToPortIdProperty = "toPort";
   initialDocumentSpot: go.Spot.TopCenter; // may work, idk
+
+  // enable Ctrl+Z to undo and Ctrl-Y to redo
+  myDiagram.undoManager.isEnabled = true;
 }
 
 function addInstruction() {
   var $ = go.GraphObject.make;
   myDiagram = $(go.Diagram, "myDiagramDiv")
 
-
   myDiagram.nodeTemplate =
-  $(go.Node, "Spot",
-  $(go.Panel, "Auto",
-    // register
-    $(go.Shape, "RoundedRectangle", {
-      fill: "lightblue",
-      desiredSize: new go.Size(100, 150),
-      //initialDocumentSpot: go.Spot.TopCenter
-    }),
-    $(go.TextBlock, { margin: 10 },
-      new go.Binding("text", "key")
-    ),
-    myDiagram.model = new go.GraphLinksModel(
-      [
-        { key: "Register" },
-        { key: "Beta" },
-      ],
-  )));
+    $(go.Node, "Position",
+      $(go.Shape, "RoundedRectangle",
+        new go.Binding("fill", "color")),
+      $(go.TextBlock,
+        { margin: 8 },
+        { alignment: new go.Spot(0, 0) },
+        new go.Binding("text", "key"))
+    );
+
+  myDiagram.model = new go.GraphLinksModel(
+    [
+      { key: "Alpha", color: "orange" },
+      { key: "Beta", color: "lime" },
+      { key: "Charlie", color: "brown" },
+    ],
+    [
+      { to: "Charlie", from: "Alpha" }
+    ]
+  )
+
+
+  // enable Ctrl+Z to undo and Ctrl-Y to redo
+  myDiagram.undoManager.isEnabled = true;
 }
 
 function storeInstruction() {
 
 }
 
-function reloadThePage(){
+function reloadThePage() {
   myDiagram.div = null;
-} 
+}
+
+
+/**
+ * Notes: We can keep the model data separate the diagram's appearance.
+ * The model data will be arrays of information that can be passed to each
+ * diagram.
+ */
