792323ea02995218809d84b959b4d667c1e48f09
Aaron Elam <speztra97@gmail.com>
2020-05-12 21:08:26 -0500

    Branch ports unfixed

--- js/app.js
+++ js/app.js
@@ -197,7 +197,7 @@ var signextendtemplate =
     $(go.Panel, "Spot",
       // register
       $(go.Shape, "Rectangle", {
-        fill: "lightorange",
+        fill: "#feb236",
         desiredSize: new go.Size(75, 100),
         //initialDocumentSpot: go.Spot.TopCenter
       }),
@@ -205,7 +205,7 @@ var signextendtemplate =
         new go.Binding("text", "key")
       )
     ),
-    // incoming sign extend
+    // incoming pc
     $(go.Shape, "Ellipse",
       {
         fill: "pink",
@@ -215,7 +215,7 @@ var signextendtemplate =
         fromLinkable: false, toLinkable: true
       }, new go.Binding("portId", "pcin")
     ),
-    // outgoing sign extend
+    // outgoing pc
     $(go.Shape, "Ellipse",
       {
         fill: "green",
@@ -307,6 +307,98 @@ var alutemplate =
     ),
   );
 
+  var addtemplate =
+  $(go.Node, "Spot",
+    {
+      movable: false,
+      copyable: false,
+      deletable: false,
+    },
+    new go.Binding("location", "loc"),  // allows changing of node's position
+    $(go.Panel, "Spot",
+      $(go.Shape, "TriangleRight", {
+        fill: "salmon",
+        desiredSize: new go.Size(60, 60),
+      }),
+      $(go.TextBlock,
+        { margin: 0 },
+        { alignmentFocus: go.Spot.Center },
+        { position: new go.Point(27, 35) }, //only way i could find to center text
+        new go.Binding("text", "key")
+      )
+    ),
+    $(go.Shape, "Ellipse",
+      {
+        fill: "pink",
+        // puts add1 left
+        alignment: new go.Spot(0, 0, 0, 20),
+        desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+        fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+        fromLinkable: false, toLinkable: true, toMaxLinks: 1
+      }, new go.Binding("portId", "add1")
+    ),
+    $(go.Shape, "Ellipse",
+      {
+        fill: "pink",
+        // puts add2 left
+        alignment: new go.Spot(0, 0, 0, 40),
+        desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+        fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+        fromLinkable: false, toLinkable: true, toMaxLinks: 1
+      }, new go.Binding("portId", "add2")
+    ),
+    $(go.Shape, "Ellipse",
+      {
+        // result on right of triangle
+        fill: "green",
+        alignment: new go.Spot(1, 0, 0, 30),
+        desiredSize: new go.Size(10, 10), //alignment: go.Spot.Right,
+        fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+        fromLinkable: true, toLinkable: false, fromMaxLinks: 1
+      }, new go.Binding("portId", "addres")
+    ),
+  );
+
+  var shiftlefttemplate = 
+  $(go.Node, "Spot",
+    {
+      movable: false,
+      copyable: false,
+      deletable: false,
+    },
+    new go.Binding("location", "loc"),  // allows changing of node's position
+    $(go.Panel, "Spot",
+      // register
+      $(go.Shape, "Ellipse", {
+        fill: "#f0b4f5",
+        desiredSize: new go.Size(75, 100),
+        //initialDocumentSpot: go.Spot.TopCenter
+      }),
+      $(go.TextBlock, { margin: 0, position: new go.Point(25, 35) },
+        new go.Binding("text", "key")
+      )
+    ),
+    // incoming shiftleft
+    $(go.Shape, "Ellipse",
+      {
+        fill: "pink",
+        alignment: new go.Spot(0, 0, 0, 50),
+        desiredSize: new go.Size(10, 10), //alignment: go.Spot.Left,
+        fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+        fromLinkable: false, toLinkable: true
+      }, new go.Binding("portId", "shiftin")
+    ),
+    // outgoing shift left
+    $(go.Shape, "Ellipse",
+      {
+        fill: "green",
+        alignment: new go.Spot(1, 0, 0, 50),
+        desiredSize: new go.Size(10, 10), //alignment: go.Spot.Right,
+        fromSpot: go.Spot.Right, toSpot: go.Spot.Left,
+        fromLinkable: true, toLinkable: false
+      }, new go.Binding("portId", "shiftout")
+    ),
+  );
 
 /*
  * 
@@ -523,12 +615,6 @@ function branchInstruction() {
 
   var $ = go.GraphObject.make;
   myDiagram = $(go.Diagram, "myDiagramDiv", {
-    // allow double-click in background to create a new node
-    //"clickCreatingTool.archetypeNodeData": { text: "Node", color: "white" },
-
-    // allow Ctrl-G to call groupSelection()
-    //"commandHandler.archetypeGroupData": { text: "Group", isGroup: true, color: "blue" },
-
     allowHorizontalScroll: false,
     allowVerticalScroll: false,
     allowZoom: false,
@@ -539,20 +625,22 @@ function branchInstruction() {
   var templmap = new go.Map(); // In TypeScript you could write: new go.Map<string, go.Node>();
   // for each of the node categories, specify which template to use
   templmap.add("register", registertemplate);
+  templmap.add("pc", programcountertemplate);
   templmap.add("im", imtemplate);
   templmap.add("signextend", signextendtemplate);
-  templmap.add("shiftleft", signextendtemplate);
+  templmap.add("shiftleft", shiftlefttemplate);
   templmap.add("alu", alutemplate);
-  templmap.add("add", alutemplate);
+  templmap.add("add", addtemplate);
   myDiagram.nodeTemplateMap = templmap;
 
   myDiagram.model = new go.GraphLinksModel(
     [
-      { key: "Register", category: "register", loc: new go.Point(-400, 50), readreg1: 0, readreg2: 1, writereg: -1, writedata: -1, readdata1: 2, readdata2: 3 },
-      { key: "IM", category: "im", loc: new go.Point(-600, 50), imfetch: 0 },
+      { key: "Register", category: "register", loc: new go.Point(-400, 50), readreg1: 1, readreg2: 2, imfetch: 0 },
+      { key: "PC", category: "pc", loc: new go.Point(-450,-100), add1: 4, addres: 7},
+      { key: "IM", category: "im", loc: new go.Point(-600, 50), readreg1: 1, readreg2: 2, sein: 3, imfetch: 0 },
       { key: "SE", category: "signextend", loc: new go.Point(-310, 250), sein: 1, seout: 4 },
       { key: "Shift Left", category: "shiftleft", loc: new go.Point(-100, -40), sein: 1, seout: 4 },
-      { key: "Add", category: "add", loc: new go.Point(50, -100), addr: 5, writedata: 3, readdata: -1 },
+      { key: "Add", category: "add", loc: new go.Point(50, -100), pcin: 7,  },
       { key: "ALU", category: "alu", loc: new go.Point(-200, 75), alu1: 2, alu2: 4, result: 5 },
     ],
   );
